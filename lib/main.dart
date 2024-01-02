5b521dedab016a6831fcb0b3a312611ec599322b
MohdAshrafk <MohdAshrafk@iprogrammer.co>
2024-01-02 11:56:45 +0530

    added config for new relic

--- lib/main.dart
+++ lib/main.dart
@@ -45,3 +45,87 @@ void main() async {
 
 
 
+// if (Platform.isIOS) {
+//     appToken = 'AAf04981a59e611b4d1feedc9afe712193530badc3-NRMA';
+// } else if (Platform.isAndroid) {
+//     appToken = 'AAbc4ecb861b0e403f907bc151f9fcf871ce878b6a-NRMA';
+// }
+
+// Config config = Config(
+//     accessToken: appToken,
+//     analyticsEventEnabled: true,
+//     networkErrorRequestEnabled: true,
+//     networkRequestEnabled: true,
+//     crashReportingEnabled: true,
+//     interactionTracingEnabled: true,
+//     httpResponseBodyCaptureEnabled: true,
+//     loggingEnabled: true,
+//     webViewInstrumentation: true,
+//     printStatementAsEventsEnabled: true,
+//     httpInstrumentationEnabled:true);
+
+// // NewrelicMobile.instance.start(config, () {
+// //   runApp(MyApp());
+// // });
+
+// runZonedGuarded(() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   FlutterError.onError = NewrelicMobile.onError;
+//   await NewrelicMobile.instance.startAgent(config);
+//   runApp(MyApp());
+// }, (Object error, StackTrace stackTrace) {
+//   NewrelicMobile.instance.recordError(error, stackTrace);
+// });
+
+
+
+
+// import 'package:newrelic_mobile/newrelic_mobile.dart';
+
+// var appToken = "";
+// if (Platform.isIOS) {
+//     appToken = '<YOUR_IOS_TOKEN>';
+// } else if (Platform.isAndroid) {
+//     appToken = '<YOUR_ANDROID_TOKEN>';
+// }
+
+// Config config =
+//     Config(accessToken: appToken,
+
+//     // Android specific option
+//     // Optional: Enable or disable collection of event data.
+//     analyticsEventEnabled: true,
+
+//     // iOS specific option
+//     // Optional: Enable or disable automatic instrumentation of WebViews.
+//     webViewInstrumentation: true,
+
+//     // Optional: Enable or disable reporting successful HTTP requests to the MobileRequest event type.
+//     networkErrorRequestEnabled: true,
+
+//     // Optional: Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
+//     networkRequestEnabled: true,
+
+//     // Optional: Enable or disable crash reporting.
+//     crashReportingEnabled: true,
+
+//     // Optional: Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
+//     interactionTracingEnabled: true,
+
+//     // Optional: Enable or disable capture of HTTP response bodies for HTTP error traces, and MobileRequestError events.
+//     httpResponseBodyCaptureEnabled: true,
+
+//     // Optional: Enable or disable agent logging.
+//     loggingEnabled: true,
+    
+//     // Optional: Enable or disable print statements as Analytics Events.
+//     printStatementAsEventsEnabled : true,
+
+//     // Optional: Enable or disable automatic instrumentation of HTTP requests.
+//     httpInstrumentationEnabled: true
+//     );
+
+// NewrelicMobile.instance.start(config, () {
+//     runApp(MyApp());
+// });
+
